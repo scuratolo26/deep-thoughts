0ab5b3ea74c0d3be7ee5415e18e81c14ea5468fe
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-10-31 21:19:55 -0400

    21.2.7 graphQL mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,5 @@
 const { gql } = require('apollo-server-express');
-// test
+
 const typeDefs = gql`
   type User {
     _id: ID
@@ -26,11 +26,25 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
